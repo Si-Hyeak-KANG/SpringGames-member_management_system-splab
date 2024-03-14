260da8706708656ee10434be99d5b804e64b4bb6
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-03-14 16:39:54 +0900

    [first init] 초기 개발환경 설정 #1

---
+++ src/test/java/test/splab/springgames/SpringGamesApplicationTests.java
@@ -0,0 +1,15 @@
+package test.splab.springgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class SpringGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
