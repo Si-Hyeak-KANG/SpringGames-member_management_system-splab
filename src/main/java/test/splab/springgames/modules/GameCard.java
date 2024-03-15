4b313af29f1579ee2333e590ed092322c4809316
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-03-15 15:07:49 +0900

    Game Card 엔티티 생성 #2

---
+++ src/main/java/test/splab/springgames/modules/GameCard.java
@@ -0,0 +1,43 @@
+package test.splab.springgames.modules;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class GameCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gameCardId;
+
+    private String title;
+
+    private Long serialNumber;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void addMember(Member member) {
+        this.member=member;
+
+    }
+
+    public void addGame(Game game) {
+        this.game = game;
+    }
+
+    public void addMemberIfNotExists(Member member) {
+        if(this.member==null) {
+            addMember(member);
+        }
+    }
+
+}
