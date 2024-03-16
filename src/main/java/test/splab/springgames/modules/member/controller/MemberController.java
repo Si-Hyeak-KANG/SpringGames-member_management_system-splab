63b1905a89138619152a1729a93ee3bad183c84c
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-03-16 23:21:04 +0900

    불필요한 annotation 정리 #6

--- src/main/java/test/splab/springgames/modules/member/controller/MemberController.java
+++ src/main/java/test/splab/springgames/modules/member/controller/MemberController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
