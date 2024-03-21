b4232fb7dd1e432106af9dc4a883005954449cb5
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-03-21 16:43:45 +0900

    존재하지 않는 카드에 대한 예외 코드 추가 #16

--- src/main/java/test/splab/springgames/exception/ExceptionCode.java
+++ src/main/java/test/splab/springgames/exception/ExceptionCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
-    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게임입니다.");
+    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게임입니다."),
+    CARD_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 카드입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
