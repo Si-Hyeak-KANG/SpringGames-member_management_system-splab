6b7b7484ca3855012583bdc960d00144ff815f4a
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-03-27 13:49:39 +0900

    잘못된 파라미터 및 기타 예외 처리 로직 추가 #25

--- src/main/java/test/splab/springgames/exception/ExceptionCode.java
+++ src/main/java/test/splab/springgames/exception/ExceptionCode.java
@@ -11,7 +11,10 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게임입니다."),
     CARD_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 카드입니다."),
-    MEMBER_BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 값으로 요청하였습니다.");
+    MEMBER_BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 값으로 요청하였습니다."),
+    PARAMETER_BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 값으로 요청하였습니다."),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "문제가 발생하였습니다. 서버에 문의해주세요.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/test/splab/springgames/exception/GlobalExceptionHandlerAdvice.java
+++ src/main/java/test/splab/springgames/exception/GlobalExceptionHandlerAdvice.java
@@ -1,8 +1,11 @@
 package test.splab.springgames.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.servlet.ModelAndView;
 import test.splab.springgames.exception.dto.ExceptionDto;
 
@@ -13,10 +16,33 @@ public class GlobalExceptionHandlerAdvice {
     @ExceptionHandler(BusinessLogicException.class)
     public ModelAndView handleBusinessLogicException(BusinessLogicException e) {
         ExceptionCode exceptionCode = e.getExceptionCode();
-        log.info("reason= [{}] {}", exceptionCode.getName(), exceptionCode.getMessage());
+        log.error("reason= [{}] {}", exceptionCode.getName(), exceptionCode.getMessage());
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("exception", ExceptionDto.from(exceptionCode));
         modelAndView.setStatus(e.getExceptionCode().getHttpStatus());
+        return modelAndView;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ModelAndView handleIllegalArgumentException(IllegalArgumentException e) {
+        ExceptionCode exceptionCode =
+                new BusinessLogicException(ExceptionCode.PARAMETER_BAD_REQUEST).getExceptionCode();
+        log.error("reason= [{}] {}", exceptionCode.getName(), exceptionCode.getMessage());
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("exception", ExceptionDto.from(exceptionCode));
+        modelAndView.setStatus(exceptionCode.getHttpStatus());
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ModelAndView handleIllegalArgumentException(Exception e) {
+        ExceptionCode exceptionCode =
+                new BusinessLogicException(ExceptionCode.INTERNAL_SERVER_ERROR).getExceptionCode();
+        log.error("reason= [{}] {}", exceptionCode.getName(), exceptionCode.getMessage());
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("exception", ExceptionDto.from(exceptionCode));
+        modelAndView.setStatus(exceptionCode.getHttpStatus());
         return modelAndView;
     }
 }
