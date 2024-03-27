205f58bfe9fdbcea434c03ae5626429f3d183176
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-03-27 13:50:24 +0900

    카드등록시, 숫자 입력에 대한 예외 처리를 위해 타입 변경 #25

--- src/main/java/test/splab/springgames/modules/card/dto/CardEnrollFormDto.java
+++ src/main/java/test/splab/springgames/modules/card/dto/CardEnrollFormDto.java
@@ -1,9 +1,6 @@
 package test.splab.springgames.modules.card.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,11 +22,11 @@ public class CardEnrollFormDto {
     String title;
 
     @Min(value = 1, message = "일련번호는 1 이상의 숫자만 허용합니다.")
-    Long serialNumber;
+    long serialNumber;
 
     @Min(value = 0, message = "가격은 $0 이상이어야 합니다.")
     @Max(value = 100_000, message = "가격은 $100,000 이하의 달러만 허용합니다.")
-    Double price;
+    double price;
 
     public GameCard toEntity() {
         return GameCard.of(this.title, this.serialNumber, this.price);
